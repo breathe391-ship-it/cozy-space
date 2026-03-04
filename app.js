c3fb038adc1bbbccb86219cba37986c04a989a20
breathe391-ship-it <breathe391@gmail.com>
2026-03-05 01:29:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 // --------------------
 // ROOM SETUP
@@ -8,7 +8,7 @@ const urlParams = new URLSearchParams(window.location.search);
 let roomName = urlParams.get("room");
 
 if (!roomName) {
-  roomName = "cozy-" + Math.random().toString(36).substring(2,8);
+  roomName = "cozy-" + Math.random().toString(36).substring(2, 8);
   window.location.search = "?room=" + roomName;
 }
 
@@ -36,23 +36,35 @@ async function assignHost() {
   }
 }
 
-await assignHost();
-
 // --------------------
-// JAAAS VIDEO SETUP
+// SECURE JITSI SETUP (JWT FROM BACKEND)
 // --------------------
 
-const domain = "8x8.vc";
+async function startJitsi() {
+  const response = await fetch("/api/generate-jwt");
+  const data = await response.json();
 
-const options = {
-  roomName: "vpaas-magic-cookie-015146b38a084d01b3812df4bd5863ec/" + roomName,
-  parentNode: document.querySelector('#video-container'),
-  width: "100%",
-  height: "100%",
-  jwt: "eyJraWQiOiJ2cGFhcy1tYWdpYy1jb29raWUtMDE1MTQ2YjM4YTA4NGQwMWIzODEyZGY0YmQ1ODYzZWMvZTExMDVlLVNBTVBMRV9BUFAiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6ImNoYXQiLCJpYXQiOjE3NzI2MzgxMzQsImV4cCI6MTc3MjY0NTMzNCwibmJmIjoxNzcyNjM4MTI5LCJzdWIiOiJ2cGFhcy1tYWdpYy1jb29raWUtMDE1MTQ2YjM4YTA4NGQwMWIzODEyZGY0YmQ1ODYzZWMiLCJjb250ZXh0Ijp7ImZlYXR1cmVzIjp7ImxpdmVzdHJlYW1pbmciOnRydWUsImZpbGUtdXBsb2FkIjp0cnVlLCJvdXRib3VuZC1jYWxsIjp0cnVlLCJzaXAtb3V0Ym91bmQtY2FsbCI6ZmFsc2UsInRyYW5zY3JpcHRpb24iOnRydWUsImxpc3QtdmlzaXRvcnMiOmZhbHNlLCJyZWNvcmRpbmciOnRydWUsImZsaXAiOmZhbHNlfSwidXNlciI6eyJoaWRkZW4tZnJvbS1yZWNvcmRlciI6ZmFsc2UsIm1vZGVyYXRvciI6dHJ1ZSwibmFtZSI6ImJyZWF0aGUzOTEiLCJpZCI6Imdvb2dsZS1vYXV0aDJ8MTA4MzA3ODgyNjIyMzIwNjIyNTA0IiwiYXZhdGFyIjoiIiwiZW1haWwiOiJicmVhdGhlMzkxQGdtYWlsLmNvbSJ9fSwicm9vbSI6IioifQ.c0pZXxAD8jg7NfiAjK3HLY9UkMCobAKbvprnf1qkX3WteA07We-3D80aubXq_eXcCyUCn-lIB0JFNrG6ym2TKRxfkbekyExuycpcDS5Z04eAo4sfXhlKgMkP1Hy6j6MFWVqUU2HruYZQ6HzKmEms5OW9D_4iskHhIY_nJh47yFWVrilrZh4yPX6JvNStZTu4vmQ6CG0mRER8xPnEOVFJj4N-I6q8e3KoxO5qzmnE38A-ynjRiytGH_9CpzfOvXkSfoXatbAmhKumGnB-2YUDG2nfOLS9h1Z2cs46EtJzeam6X84v9pRNmjdlpUKcktLuvQy68xzsKkZ5mhLEsKCQCA"
-};
+  const options = {
+    roomName: "vpaas-magic-cookie-015146b38a084d01b3812df4bd5863ec/" + roomName,
+    parentNode: document.querySelector("#video-container"),
+    width: "100%",
+    height: "100%",
+    jwt: data.token
+  };
 
-const api = new JitsiMeetExternalAPI(domain, options);
+  new JitsiMeetExternalAPI("8x8.vc", options);
+}
+
+// --------------------
+// INITIALIZE APP (ORDER MATTERS)
+// --------------------
+
+async function init() {
+  await assignHost();
+  await startJitsi();
+}
+
+init();
 
 // --------------------
 // MUSIC DRAWER TOGGLE
@@ -66,7 +78,7 @@ toggleBtn.onclick = () => {
 };
 
 // --------------------
-// LOCAL QUEUE SYSTEM (TEMPORARY - WILL SYNC LATER)
+// LOCAL QUEUE SYSTEM
 // --------------------
 
 let queue = [];
@@ -112,11 +124,11 @@ function renderQueue() {
 // YOUTUBE PLAYER
 // --------------------
 
-window.onYouTubeIframeAPIReady = function() {
-  player = new YT.Player('player', {
-    height: '200',
-    width: '100%',
-    videoId: '',
+window.onYouTubeIframeAPIReady = function () {
+  player = new YT.Player("player", {
+    height: "200",
+    width: "100%",
+    videoId: ""
   });
 };
 
